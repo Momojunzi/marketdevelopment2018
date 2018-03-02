ca5c074cbb8f46f59e4028bca58806dde8abf411
momojunzi <mark.holthaus@gmail.com>
2018-03-01 20:03:23 -0800

    the future

--- src/containers/future/future.js
+++ src/containers/future/future.js
@@ -14,7 +14,7 @@ class Future extends Component {
         <PageHeader headerText="The Future" />
         <main >
           <div style={mainStyle} className="row justify-content-center">
-
+              <h3>We Live In The Present But The Future Is - Coming Soon...</h3>
               <BackButtons back="/" />
           </div>
         </main>
